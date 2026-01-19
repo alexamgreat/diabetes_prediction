962e7450a49fbdb0c34f6c265455121043467d21
alexamgreat <alexamgreat@gmail.com>
2026-01-19 15:07:19 +0700

    model training and prediction

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 import logging
-
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -16,6 +16,10 @@ class CustomException(Exception):
         self.error_message = error_message_detail(error_message, error_detail)
 
     def __str__(self):
-        return self.error_message   
+        return self.error_message 
+    
+    
+   
+          
     
     
